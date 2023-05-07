32ab13194aa6ad9ba4895da3fc5ff4836f0eefb9
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-05-07 11:35:46 +0100

    prevent leaks and freed the command

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 09:47:07 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/05/07 10:18:07 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/05/07 11:16:44 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,13 @@ int	main(int ac, char **ag)
 	{
 		cmd = prompt_string();
 		if (!cmd)
+		{
+			printf("None");
 			cmd = prompt_string();
+		}
 		else
 			printf("%s\n", cmd);
+		free(cmd);
 	}
 	return (0);
 }
