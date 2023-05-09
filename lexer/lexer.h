6d9635a2c3ab89c6acb54a66d1fac8ccef31f3ad
amaitou <amait-ou@student.1337.ma>
2023-05-09 16:46:48 +0100

    fixed readline lib on ubuntu

--- lexer/lexer.h
+++ lexer/lexer.h
@@ -6,15 +6,14 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 11:37:36 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/05/09 14:37:35 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/05/09 16:46:12 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEXER_H
 # define LEXER_H
 
-# include "../superlib/superlib.h"
-# include <stdio.h>
+# include "../minishell.h"
 
 char	*handle_quotes(char *p, int *q, char c);
 char	*split_command(char *s);
