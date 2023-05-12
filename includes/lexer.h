3520e662fda7a45d36efa7e2bf909528f3cd65bd
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-05-12 20:55:11 +0100

    until now the tokenizer looks good

--- includes/lexer.h
+++ includes/lexer.h
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   lexer.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbouagou <bbouagou@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/07 11:37:36 by amait-ou          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/05/12 17:28:16 by amait-ou         ###   ########.fr       */
-=======
-/*   Updated: 2023/05/11 04:47:03 by bbouagou         ###   ########.fr       */
->>>>>>> 41c761d0d448382e8165c2225a6c1b70b3ac6205
+/*   Created: 2023/05/12 20:50:35 by amait-ou          #+#    #+#             */
+/*   Updated: 2023/05/12 20:53:56 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +14,6 @@
 # define LEXER_H
 
 # include "./minishell.h"
-
-<<<<<<< HEAD
 typedef struct s_lexer
 {
 	int		i;
@@ -32,10 +26,7 @@ void	quotes(t_lexer *lexer, char *s);
 void	operators(t_lexer *lexer, char *s);
 char 	*scanner(char *s);
 char	**tokenizer(char *s);
-=======
-/**
- * @brief An enum which has all control operators for a command.
- */
+void	tokens_traversal(char **t);
 
 typedef enum e_cntrl_op
 {
@@ -46,9 +37,6 @@ typedef enum e_cntrl_op
 	PIPE
 }	t_cntrl_op;
 
-/**
- * @brief A linked list containing commands and their arguments.
- */
 
 typedef struct s_info
 {
@@ -56,10 +44,5 @@ typedef struct s_info
 	t_cntrl_op	op;
 }	t_info;
 
-char	*handle_quotes(char *p, int *q, char c);
-char	*split_command(char *s);
-char	**create_tokens(char *s);
->>>>>>> 41c761d0d448382e8165c2225a6c1b70b3ac6205
-void	tokens_traversal(char **t);
 
 #endif
