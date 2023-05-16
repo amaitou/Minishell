9d4507bdb02c42574e0b6260e3fc099f9157e832
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-05-16 18:19:28 +0100

    removed the free_struct function as it peforms troubles

--- sources/lexer/utils.c
+++ sources/lexer/utils.c
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 20:51:54 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/05/16 15:37:03 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/05/16 18:03:50 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,6 @@ void	tokens_traversal(char **tokenizer)
 		printf(" [.] %s\n", tokenizer[i]);
 }
 
-void	free_struct(t_lexer *lexer)
-{
-	int	i;
-
-	i = -1;
-	free (lexer->cmd);
-	while (lexer->tokens[++i])
-		free(lexer->tokens[i]);
-	free(lexer->tokens);
-	free(lexer);
-}
 
 void	free_array(char **arr)
 {
@@ -42,3 +31,10 @@ void	free_array(char **arr)
 		free(arr[i]);
 	free(arr);
 }
+
+// void	free_struct(t_lexer *lexer)
+// {
+// 	free (lexer->cmd);
+// 	free_array(lexer->tokens);
+// 	free(lexer);
+// }
