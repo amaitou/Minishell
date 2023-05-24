d93ae7fbfd2f7c1c7a2638ce20546b81fb5076af
amaitou <amait-ou@student.1337.ma>
2023-05-24 22:43:18 +0100

    get back to the old init_env function

--- sources/builtins/init_env.c
+++ sources/builtins/init_env.c
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 07:05:43 by bbouagou          #+#    #+#             */
-/*   Updated: 2023/05/24 19:35:30 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/05/24 22:42:42 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,43 +22,43 @@ static int	get_v(char *string)
 	return (i);
 }
 
-// t_env	*init_env(t_env	*env, char **envp)
-// {
-// 	int		i;
-// 	t_env	*traveser;
-
-// 	i = 0;
-// 	env = ft_lstnew();
-// 	traveser = env;
-// 	while (envp[i])
-// 	{
-// 		traveser->key = ft_substr(envp[i], 0, get_v(envp[i]));
-// 		traveser->value = ft_substr(envp[i],
-// 				get_v(envp[i]) + 1, ft_strlen(envp[i]));
-// 		if (envp[i + 1])
-// 			ft_lstadd_back(&env, ft_lstnew());
-// 		traveser = traveser->next;
-// 		i++;
-// 	}
-// 	return (env);
-// }
-
 t_env	*init_env(t_env	*env, char **envp)
 {
 	int		i;
-	t_env	*head;
-	t_env	*node;
+	t_env	*traveser;
 
 	i = 0;
-	(void)env;
+	env = ft_lstnew();
+	traveser = env;
 	while (envp[i])
 	{
-		node = ft_lstnew();
-		node->key = ft_substr(envp[i], 0, get_v(envp[i]));
-		node->value = ft_substr(envp[i],
+		traveser->key = ft_substr(envp[i], 0, get_v(envp[i]));
+		traveser->value = ft_substr(envp[i],
 				get_v(envp[i]) + 1, ft_strlen(envp[i]));
-		ft_lstadd_back(&head, node);
+		if (envp[i + 1])
+			ft_lstadd_back(&env, ft_lstnew());
+		traveser = traveser->next;
 		i++;
 	}
-	return (head);
+	return (env);
 }
+
+// t_env	*init_env(t_env	*env, char **envp)
+// {
+// 	int		i;
+// 	t_env	*head;
+// 	t_env	*node;
+
+// 	i = 0;
+// 	(void)env;
+// 	while (envp[i])
+// 	{
+// 		node = ft_lstnew();
+// 		node->key = ft_substr(envp[i], 0, get_v(envp[i]));
+// 		node->value = ft_substr(envp[i],
+// 				get_v(envp[i]) + 1, ft_strlen(envp[i]));
+// 		ft_lstadd_back(&head, node);
+// 		i++;
+// 	}
+// 	return (head);
+// }
