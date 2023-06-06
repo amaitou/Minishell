1197c76ed3cca1498416ec5bc6585b09b3de5fbf
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-06-06 19:57:18 +0100

    fixed all norm issues

--- sources/parsing/lexer.c
+++ sources/parsing/lexer.c
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 16:57:38 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/06/04 01:13:10 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/06/06 19:56:49 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	tokenizer(t_dlist **head, char *token)
 			append_node(head, node_quotes(token, '\"'));
 	}
 	else if (!ft_strcmp(token, ">") || !ft_strcmp(token, "<")
-			|| !ft_strcmp(token, ">>") || !ft_strcmp(token, "<<"))
+		|| !ft_strcmp(token, ">>") || !ft_strcmp(token, "<<"))
 		append_node(head, node_redirection(token));
 	else if (!ft_strcmp(token, "|"))
 		append_node(head, node_pipeline(token));
@@ -30,7 +30,7 @@ void	tokenizer(t_dlist **head, char *token)
 		append_node(head, node_word(token));
 }
 
-void	__lexer(t_dlist **head, t_scanner *scanner)
+void	__lexer__(t_dlist **head, t_scanner *scanner)
 {
 	scanner->i = 0;
 	while (scanner->tokens[scanner->i])
