74b6fcade2e65827b11ae7d73149377aa36ed48b
Bilal Bouagou <bilalbouagou@protonmail.com>
2023-06-09 23:57:51 +0100

    oga boga pipes sorta implemented

--- execution/executor.c
+++ execution/executor.c
@@ -6,7 +6,7 @@
 /*   By: bbouagou <bbouagou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 01:47:00 by bbouagou          #+#    #+#             */
-/*   Updated: 2023/06/07 22:28:16 by bbouagou         ###   ########.fr       */
+/*   Updated: 2023/06/09 23:57:18 by bbouagou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,69 @@ static int	exec_builtins(t_dlist *list, char *env[])
 		return (0);
 }
 
+static int	ft_pipe(t_dlist *list, char *env[], int pipefd[2])
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (!pid)
+	{
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[1]);
+		if (execve(list->cmd, list->args, env) == -1)
+		{
+			perror("ft_pipe : ");
+			exit(EXIT_FAILURE);
+		}
+	}
+	list = list->next;
+	pid = fork();
+	if (!pid)
+	{
+		close(pipefd[1]);
+		dup2(pipefd[0], STDIN_FILENO);
+		close(pipefd[0]);
+		if (execve(list->cmd, list->args, env) == -1)
+		{
+			perror("ft_pipe : ");
+			exit(EXIT_FAILURE);
+		}
+	}
+	close(pipefd[0]);
+	close(pipefd[1]);
+	return (0);
+}
+
 void	executor(t_dlist *list, int *status, char *env[])
 {
 	t_dlist	*tmp;
+	int		pipefd[2];
 
 	tmp = list;
 	while (tmp)
 	{
-		if (tmp->builtin != _NONE)
-			*status = exec_builtins(tmp, env);
-		else
-			*status = exec_cmd(tmp, env);
+		if (tmp->type == __PIPE)
+		{
+			pipe(pipefd);
+			*status = ft_pipe(tmp, env, pipefd);
+		}
+		// else if (tmp->builtin != _NONE)
+		// 	*status = exec_builtins(tmp, env);
+		// else
+		// 	*status = exec_cmd(tmp, env);
 		tmp = tmp->next;
 	}
 }
+
+/*
+
+fd[1] -> fd[0]
+
+parent : fd[2]
+child1: fd[2] -> close fd[0] | dup2 fd[1], stdout | close fd[1];
+close(fd[1])
+child2 : fd[2] > close fd[1] | dup2 fd[0], stdin | close fd[0];
+close(fd[0])
+
+*/
--- execution/main.c
+++ execution/main.c
@@ -6,7 +6,7 @@
 /*   By: bbouagou <bbouagou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 21:54:43 by bbouagou          #+#    #+#             */
-/*   Updated: 2023/06/07 22:25:30 by bbouagou         ###   ########.fr       */
+/*   Updated: 2023/06/09 23:54:46 by bbouagou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,20 @@
 
 int	main(void)
 {
-	t_dlist *list;
+	t_dlist	*list;
 	int		status;
 
 	list = create_node();
-	list->cmd = ft_strdup("pwd");
-	list->args = ft_split("hello world", ' ');
-	list->builtin = __PWD;
-	list->type = __NONE;
+	list->cmd = ft_strdup("/usr/bin/yes");
+	list->args = ft_split("yes", ' ');
+	list->builtin = _NONE;
+	list->type = __PIPE;
 	list->state = __D_QUOTES;
+	append_node(&list, create_node());
+	list->next->cmd = ft_strdup("/usr/bin/head");
+	list->next->args = ft_split("head", ' ');
+	list->next->builtin = _NONE;
+	list->next->type = __NONE;
+	list->next->state = __D_QUOTES;
 	executor(list, &status, NULL);
 }
