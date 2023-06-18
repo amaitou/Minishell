fd0730055828bd42090af5e2c734d64375ed66f4
Bilal Bouagou <bilalbouagou@protonmail.com>
2023-06-18 12:38:14 +0100

    fixed wildcards

--- sources/parsing/wilcards_expander_utils.c
+++ sources/parsing/wilcards_expander_utils.c
@@ -6,23 +6,35 @@
 /*   By: bbouagou <bbouagou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 13:29:19 by bbouagou          #+#    #+#             */
-/*   Updated: 2023/06/14 13:35:01 by bbouagou         ###   ########.fr       */
+/*   Updated: 2023/06/18 12:36:30 by bbouagou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static int	search(t_dlist *list, int i, int j)
+static int	search(t_dlist *list, int i, int *j)
 {
-	if (!i && list->first && !ft_strnstr(list->p_match + j,
+	if (!i && list->first && !ft_strnstr(list->p_match + *j,
 			list->req[i], ft_strlen(list->req[i])))
+	{
+		*j = (int)(list->p_match - ft_strnstr(list->p_match + *j, list->req[i],
+					ft_strlen(list->req[i])));
 		return (0);
+	}
 	else if (!list->req[i + 1] && list->last
-		&& !ft_strlaststr(list->p_match + j, list->req[i]))
+		&& !ft_strlaststr(list->p_match + *j, list->req[i]))
+	{
+		*j = (int)(list->p_match - ft_strlaststr(list->p_match + *j,
+					list->req[i]));
 		return (0);
-	else if (!ft_strnstr(list->p_match + j, list->req[i],
+	}
+	else if (!ft_strnstr(list->p_match + *j, list->req[i],
 			ft_strlen(list->p_match)))
+	{
+		*j = (int)(list->p_match - ft_strnstr(list->p_match + *j, list->req[i],
+					ft_strlen(list->p_match)));
 		return (0);
+	}
 	return (1);
 }
 
@@ -54,11 +66,8 @@ void	search_for_match(t_dlist *list)
 	if (list->p_match[0] == '.')
 		return ;
 	while (list->req[++i])
-	{
-		if (!search(list, i, j))
+		if (!search(list, i, &j))
 			break ;
-		j = ft_strlen(list->req[i]);
-	}
 	if (!list->req[i])
 	{
 		list->matched = string_join(list->matched, list->p_match);
