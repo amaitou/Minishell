bc074ae92308f5f93b5b14b3694b003db8d170a5
Bilal Bouagou <bilalbouagou@protonmail.com>
2023-06-19 13:06:20 +0100

    fixed segfault in prompt string

--- sources/prompt_string.c
+++ sources/prompt_string.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: bbouagou <bbouagou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/12 20:52:51 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/06/19 10:20:53 by bbouagou         ###   ########.fr       */
+/*   Created: 2023/06/19 12:59:13 by bbouagou          #+#    #+#             */
+/*   Updated: 2023/06/19 13:03:43 by bbouagou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,15 @@ void	get_user(t_prompt *prompt)
 	char	*cwd;
 
 	user = getenv("USER");
-	cwd = getcwd(NULL, PATH_MAX);
-	prompt->line = string_join(ft_strdup("[\033[0;32m"), ft_strdup(user));
+	cwd = getcwd(NULL, 0);
+	prompt->line = NULL;
+	if (cwd == NULL)
+	{
+		prompt->line = ft_strdup("\033[0;36mminishell\033[0m]$ ");
+		return ;
+	}
+	if (user)
+		prompt->line = string_join(ft_strdup("[\033[0;32m"), ft_strdup(user));
 	prompt->line = string_join(prompt->line,
 			ft_strdup("\033[0m\033[1;37m@1337\033[0m\033[0;36m "));
 	prompt->line = string_join(prompt->line,
@@ -30,11 +37,6 @@ void	get_user(t_prompt *prompt)
 
 char	*prompt_string(t_prompt *prompt)
 {
-	if (getenv("USER"))
-	{
-		get_user(prompt);
-		return (readline(prompt->line));
-	}
-	else
-		return (readline("\033[0;36mminishell\033[0m]$ "));
+	get_user(prompt);
+	return (readline(prompt->line));
 }
