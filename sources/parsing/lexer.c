117059f1ec8a4b32116f2254f09d17f992e9f99f
Amine Ait Ouazghour <amineotaku239@gmail.com>
2023-06-23 12:14:01 +0100

    replaced null in lexer with null-character

--- sources/parsing/lexer.c
+++ sources/parsing/lexer.c
@@ -6,7 +6,7 @@
 /*   By: amait-ou <amait-ou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 10:37:57 by amait-ou          #+#    #+#             */
-/*   Updated: 2023/06/21 18:37:55 by amait-ou         ###   ########.fr       */
+/*   Updated: 2023/06/23 12:11:08 by amait-ou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,39 @@ void	tokenizer(t_dlist **lexer, char *token)
 		append_node(lexer, node_word(token));
 }
 
-void	assign_token(t_dlist *lexer, char *token)
+void	assign_token(t_dlist **lexer, char *token)
 {
+	free((*lexer)->value);
 	if (token)
-		lexer->value = ft_strdup(token);
+		(*lexer)->value = ft_strdup(token);
 	else
-		lexer->value = NULL;
+		(*lexer)->value = NULL;
+	(*lexer) = (*lexer)->next;
+	free(token);
+}
+
+void	join_chars(t_dlist *lexer, int *i, char **token)
+{
+	char	quote;
+
+	if (lexer->value[*i] == '\'' || lexer->value[*i] == '\"')
+	{
+		quote = lexer->value[(*i)++];
+		if (lexer->value[*i] == quote)
+			*token = string_join(*token, ft_strdup("\0"));
+		else
+		{
+			while (lexer->value[*i] != quote && lexer->value[*i])
+				*token = string_join(*token,
+						ft_substr(lexer->value, (*i)++, 1));
+		}
+	}
+	else
+		*token = string_join(*token, ft_substr(lexer->value, *i, 1));
 }
 
 void	quotes_removal(t_dlist *lexer)
 {
-	int		quote;
 	int		i;
 	char	*token;
 
@@ -50,20 +72,10 @@ void	quotes_removal(t_dlist *lexer)
 		token = NULL;
 		while (lexer->value[i])
 		{
-			if (lexer->value[i] == '\'' || lexer->value[i] == '\"')
-			{
-				quote = lexer->value[i++];
-				while (lexer->value[i] != quote && lexer->value[i])
-					token = string_join(token, ft_substr(lexer->value, i++, 1));
-			}
-			else
-				token = string_join(token, ft_substr(lexer->value, i, 1));
+			join_chars(lexer, &i, &token);
 			++i;
 		}
-		free(lexer->value);
-		assign_token(lexer, token);
-		lexer = lexer->next;
-		free(token);
+		assign_token(&lexer, token);
 	}
 }
 
